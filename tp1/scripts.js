8ba551427b4cc9f185578a4ec1f5700cbf3cad2a
Élise Rubio <elise.rubio@4sh.fr>
2023-12-11 18:07:17 +0100

    TP 1 step 3 solution

--- tp1/scripts.js
+++ tp1/scripts.js
@@ -1,1 +1,13 @@
 const numbers = [12, 1, 96, 5, 45];
+
+// Using a for loop
+let sum = 0;
+
+for(let n of numbers) {
+    sum += n;    
+}
+
+console.log(sum);
+
+// Using reduce
+console.log(numbers.reduce((previousValue, currentValue) => previousValue + currentValue));
