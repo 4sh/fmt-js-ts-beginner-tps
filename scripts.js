111425d7ce569727bdb3b60e2e67f7edb67fab58
Élise Rubio <elise.rubio@4sh.fr>
2023-12-14 17:18:15 +0100

    Move isInvalidStatValue inside PokemonStats

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
-function isInvalidStatValue(value) {
-    return value === undefined || value <= 0;
-}
-
 class PokemonStats {
     #attack;
     #specialAttack;
 
+    #isInvalidStatValue(value) {
+        return value === undefined || value <= 0;
+    }
+
     constructor(attack, specialAttack) {
-        if (isInvalidStatValue(attack) || isInvalidStatValue(specialAttack)) {
+        if (this.#isInvalidStatValue(attack) || this.#isInvalidStatValue(specialAttack)) {
             throw new Error('Attack and special attack should be positive and defined');
         }
         this.#attack = attack;
         this.#specialAttack = specialAttack;
     }
 }
+
