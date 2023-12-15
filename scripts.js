e1bb4cbbbe7824afb4808d66dad86897443164ac
Élise Rubio <elise.rubio@4sh.fr>
2023-12-15 14:14:58 +0100

    Prepare for tp4

--- scripts.js
+++ scripts.js
@@ -1,32 +1,15 @@
-/**
- * @param pokemonId Can be either pokemon id or pokemon name
- * @returns {string} URL to get Pokemon from PokeApi
- */
-function buildPokeApiUrl(pokemonId) {
-    return 'https://pokeapi.co/api/v2/pokemon/' + pokemonId;
+function f1(a) {
+    return f2(a**2);
 }
 
-async function getPokemon(id) {
-    const pokemonUrl = buildPokeApiUrl(id);
-
-    const response = await fetch(pokemonUrl);
-
-    if (response.status === 404) {
-        throw new Error('Pokemon was not found')
-    }
-
-    if (id % 2 === 0) {
-        return await new Promise(resolve => setTimeout(() => resolve(response.json()), 2000));
-    }
-
-    return response.json();
+function f2(b) {
+    const y = 29+f3(b/9);
+    return y*8;
 }
 
-const p1 = getPokemon(38);
-const p2 = getPokemon(261);
-const p3 = getPokemon(1303);
+function f3(c) {
+    return 2*3+Math.log(c)**4;
+}
 
-Promise.all([p1, p2]).then(values => console.log('Total height: ' + (values[0].height + values[1].height))); // Displays: 16
-Promise.any([p3, p1]).then(value => console.log('any name: ' + value.name)); // Displays: ninetales
-Promise.race([p1, p2]).then(value => console.log('race name: ' + value.name)); // Dipslays: race name: poochyena
-
+[5, 2, 3, 6, 8, 15, 12, 13, 16, 18, 15, 12, 13, 16, 18, 15, 22, 23, 26, 28, 25, 22, 33, 36, 38, 35, 32, 43, 46, 48, 45, 42, 53, 56, 58]
+    .map(x => f1(x));
