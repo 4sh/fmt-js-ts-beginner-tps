aac2799ffece50bd0d3725d7695fa29653bbd5da
Élise Rubio <elise.rubio@4sh.fr>
2023-12-15 14:14:58 +0100

    Prepare for tp3

--- scripts.js
+++ scripts.js
@@ -1,27 +1,15 @@
-class PokemonStats {
-    #attack;
-    #specialAttack;
-
-    static #isInvalidStatValue(value) {
-        return value === undefined || value <= 0;
-    }
-
-    constructor(attack, specialAttack) {
-        if (PokemonStats.#isInvalidStatValue(attack) || PokemonStats.#isInvalidStatValue(specialAttack)) {
-            throw new Error('Attack and special attack should be positive and defined');
-        }
-        this.#attack = attack;
-        this.#specialAttack = specialAttack;
-    }
-
-    incrementAttack(increment = 1) {
-        this.#attack += increment;
-    }
+function f1(a) {
+    return f2(a**2);
 }
 
-const stats = new PokemonStats(12, 45);
-console.log(stats);
-stats.incrementAttack();
-console.log(stats);
-stats.incrementAttack(5);
-console.log(stats);
+function f2(b) {
+    const y = 29+f3(b/9);
+    return y*8;
+}
+
+function f3(c) {
+    return 2*3+Math.log(c)**4;
+}
+
+[5, 2, 3, 6, 8, 15, 12, 13, 16, 18, 15, 12, 13, 16, 18, 15, 22, 23, 26, 28, 25, 22, 33, 36, 38, 35, 32, 43, 46, 48, 45, 42, 53, 56, 58]
+    .map(x => f1(x));
