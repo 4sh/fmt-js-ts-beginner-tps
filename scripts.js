71b31f70505b6830f06f5d1f3c55132e77deb614
Élise Rubio <elise.rubio@4sh.fr>
2023-12-26 10:17:48 +0100

    Prepare for tp3 step 1

--- scripts.js
+++ scripts.js
@@ -1,27 +1,17 @@
-class PokemonStats {
-    #attack;
-    #specialAttack;
+const resolvedPromiseCreatedUsingConstructor;
+const rejectedPromiseCreatedUsingConstructor;
+const resolvedPromiseCreatedUsingPromiseAPI;
+const rejectedPromiseCreatedUsingPromiseAPI;
 
-    static #isInvalidStatValue(value) {
-        return value === undefined || value <= 0;
-    }
-
-    constructor(attack, specialAttack) {
-        if (PokemonStats.#isInvalidStatValue(attack) || PokemonStats.#isInvalidStatValue(specialAttack)) {
-            throw new Error('Attack and special attack should be positive and defined');
-        }
-        this.#attack = attack;
-        this.#specialAttack = specialAttack;
-    }
-
-    incrementAttack(increment = 1) {
-        this.#attack += increment;
-    }
+function printSquare(x) {
+    return console.log(x**2);
 }
 
-const stats = new PokemonStats(12, 45);
-console.log(stats);
-stats.incrementAttack();
-console.log(stats);
-stats.incrementAttack(5);
-console.log(stats);
+function printStringLength(str) {
+    return console.log(str.length);
+}
+
+resolvedPromiseCreatedUsingConstructor.then(printSquare);
+rejectedPromiseCreatedUsingConstructor.catch(printStringLength)
+resolvedPromiseCreatedUsingPromiseAPI.then(printSquare);
+rejectedPromiseCreatedUsingPromiseAPI.catch(printStringLength)
