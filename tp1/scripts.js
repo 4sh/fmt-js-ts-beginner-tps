5497573958f61c19fb39e297cb42f7be1dfddeda
Élise Rubio <elise.rubio@4sh.fr>
2023-12-26 16:54:30 +0100

    Prepare for tp 4 step 4

--- tp1/scripts.js
+++ tp1/scripts.js
@@ -1,24 +1,16 @@
-async function buildRandomPromise() {
-    const randomNumber = Math.random();
-    if(randomNumber > 0.5) {
-        throw Error(`Oh no, the number ${randomNumber} is too high`);
-    }
-
-    return randomNumber;
+/**
+ * @param pokemonId Can be either pokemon id or pokemon name
+ * @returns {string} URL to get Pokemon from PokeApi
+ */
+function buildPokeApiUrl(pokemonId) {
+    return 'https://pokeapi.co/api/v2/pokemon/' + pokemonId;
 }
 
-async function main() {
-    for (let i = 0; i < 10; ++i) {
-        try {
-            const randomNumber = await buildRandomPromise();
-            console.log((randomNumber + 1) * 2);
-        } catch(error) {
-            console.log(error.message);
-        } finally {
-            console.log('Random promise terminated')
-        }
-    }
+function getPokemon(id) {
+    const pokemonUrl = buildPokeApiUrl(id);
+    
+    
 }
 
-main();
-
+getPokemon(598);
+getPokemon(4318431);
