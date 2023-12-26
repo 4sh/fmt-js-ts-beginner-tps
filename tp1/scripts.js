d2f607c8b5d3d7e0349622fae323db7b972bab8f
Élise Rubio <elise.rubio@4sh.fr>
2023-12-26 17:15:57 +0100

    Implement getPokemon & test it

--- tp1/scripts.js
+++ tp1/scripts.js
@@ -6,11 +6,17 @@ function buildPokeApiUrl(pokemonId) {
     return 'https://pokeapi.co/api/v2/pokemon/' + pokemonId;
 }
 
-function getPokemon(id) {
+async function getPokemon(id) {
     const pokemonUrl = buildPokeApiUrl(id);
     
+    const response = await fetch(pokemonUrl);
     
+    if (response.status === 404) {
+        throw new Error('Pokemon was not found')
+    }
+    
+    return response.json();
 }
 
-getPokemon(598);
-getPokemon(4318431);
+getPokemon(598).then(console.log);
+getPokemon(4318431).catch(console.log);
