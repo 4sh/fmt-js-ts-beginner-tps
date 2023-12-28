fcdf7299a3a0256dec8f529da62528f02388de9a
Élise Rubio <elise.rubio@4sh.fr>
2023-12-28 18:01:28 +0100

    Prepare for tp 5 step 3

--- tp5/scripts.js
+++
@@ -1,9 +0,0 @@
-import {
-    generateRandomNickname,
-    Pokemon,
-    pokemonSpecies as trainerPokemonSpecies
-} from "pokemon";
-
-const myPokemons = trainerPokemonSpecies.map(species => new Pokemon(species, generateRandomNickname(species)));
-
-console.log(myPokemons);
