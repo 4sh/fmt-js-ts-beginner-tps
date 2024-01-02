deb991b05e81dcbf802a03c93d9b6ba744992512
Élise Rubio <elise.rubio@4sh.fr>
2024-01-02 15:34:18 +0100

    Create object vulpix

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const vulpix = {
+    species: 'vulpix',
+    nickname: 'Lil fox',
+    stats: {
+        'attack:':41,
+        'special attack': 50
+    }
+}
+
+console.log(vulpix);
+console.log(vulpix.nickname);
+console.log(vulpix.stats["special attack"]);
+
+vulpix.level = 5
+
+console.log(vulpix);
