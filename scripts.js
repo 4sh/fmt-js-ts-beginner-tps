e9b3f1e4de21f891744baab45659321bef802a3b
Élise Rubio <elise.rubio@4sh.fr>
2024-01-04 17:35:26 +0100

    Solution for TP6.1 p2

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ function onResetButtonClick() {
 function onNumberInputClick() {
     console.log('Number input was clicked!');
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const numberInputElement = document.getElementById('numberInput');
+    numberInputElement.addEventListener('click', onNumberInputClick);
+})
