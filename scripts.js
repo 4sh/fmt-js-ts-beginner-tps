86868506d750abc03f1dfbb5da7c5b51d08eed0f
Élise Rubio <elise.rubio@4sh.fr>
2024-01-04 19:21:11 +0100

    Compute square & display warning when square is too high

--- scripts.js
+++ scripts.js
@@ -2,8 +2,22 @@ function computeSquare(x) {
     return Number(x**2);
 }
 
+function updateSquareValue(newValue) {
+    document.getElementById('square').innerText = newValue.toString();
+}
+
 const squareIsBiggerThanOneThousandMessage = 'Le carré du nombre est plus grand que 1000 !';
+const squareIsBiggerThanOneThousandMessageId= 'squareValueWarning';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const numberInputElement = document.getElementById('numberInput');
+    numberInputElement.addEventListener('change', () => {
+        const square = computeSquare(numberInputElement.value);
+        updateSquareValue(square);
+    })
+    document.getElementById('resetButton').addEventListener('click', () => {
+        numberInputElement.value = 0;
+        updateSquareValue(0);
+    })
 
 })
