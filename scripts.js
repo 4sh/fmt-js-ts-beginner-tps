5aaf4c50400a6d2897adc1ea15122d99899c787c
Frédéric Camblor <frederic.camblor@4sh.fr>
2025-09-08 14:50:54 +0200

    TP 1.2 P3 bonus solution

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 const numbers = [12, 1, 96, 5, 45];
 
-// Using for loop
-let minimum = numbers[0];
+// Using Array.reduce()
+const reduceMinimum = numbers.reduce((min, n) => n < min ? n : Infinity);
+console.log(reduceMinimum);
 
-for (let n of numbers) {
-    if (n < minimum) {
-        minimum = n;
-    }
-}
+// Using Math.min()
+// Math.min() can take multiple parameters (Math.min(3,6,2,4) => 2)
+// Here, we're using spread operator (...) to transform array into parameters
+const mathMinimum1 = Math.min(...numbers);
+console.log(mathMinimum1);
+// We can also call prototype's function apply()
+const mathMinimum2 = Math.min.apply(null, numbers);
+console.log(mathMinimum2);
 
-console.log(minimum);
+// Using Array.sort()
+// warning: Array.sort() mutates underlying array !
+const sortedNumbers = numbers.sort((n1, n2) => n1 - n2);
+console.log(sortedNumbers[0]);
+// Beware: after that, numbers is updated !
+
